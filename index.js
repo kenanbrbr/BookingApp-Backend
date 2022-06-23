608e1100ebf31b6a697a8ae52a89307bbd1cf1ff
kberber <kenanbrbr@gmail.com>
2022-06-23 21:42:23 +0300

    error useage example

--- index.js
+++ index.js
@@ -42,7 +42,16 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-
+app.use((err, req, res, next) => {
+  const errStatus = err.status || 500;
+  const errMessage = err.message || "Something went wrong....";
+  return res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: err.stack
+  })
+})
 
 app.listen(8800, () => {
   connect()
